911761d4d6ce1e493c5ef365638c88e4dc886327
Andriistudent <S2110769010@fhooe.at>
2021-11-14 23:35:00 +0100

    adds the subtask1.py

---
+++ subtask1.py
@@ -0,0 +1,12 @@
+def lar_sq (n):
+    a = 0
+    while (a**2) <= n:
+        a += 1
+    q = (a-1)**2
+    print(q)
+
+lar_sq (5)
+lar_sq (9)
+lar_sq (99)
+
+# it looks like I learned how to use git today
